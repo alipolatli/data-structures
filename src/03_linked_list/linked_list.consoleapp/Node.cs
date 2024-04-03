3ee6c56db6196cee53c27aeaa1a85ae06aa5b504
Ali Polatlı <ali.polatli@outlook.com>
2024-04-03 17:43:12 +0300

    feat: add linkedlist.

---
+++ src/03_linked_list/linked_list.consoleapp/Node.cs
@@ -0,0 +1,15 @@
+namespace linked_list.consoleapp;
+
+public class Node<T>
+{
+    public T Data { get; set; }
+
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+    }
+
+    public override string ToString() => $"Data: {Data}";
+}
--- src/03_linked_list/linked_list.consoleapp/Program.cs
+++ src/03_linked_list/linked_list.consoleapp/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        CustomLinkedList<int> list = new();
+
+        list.AddFirst(1);
+        list.AddLast(2);
+        list.AddLast(new[] { 3, 4, 5 });
+        list.AddMiddle(0);
+        list.AddFirst(31);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
