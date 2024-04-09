50d58be5b5da899d79319b6dae2567a836b5bfe3
Ali Polatlı <ali.polatli@outlook.com>
2024-04-09 22:40:06 +0300

    feat: add hashcode and equals methods review project.

---
+++ src/06_hash/equals.consoleapp/Program.cs
@@ -0,0 +1,26 @@
+﻿MyClass obj1 = new MyClass { ValueA = 5, ValueB = 1 };
+MyClass obj2 = new MyClass { ValueA = 5, ValueB = 10 };
+MyClass obj3 = obj1;
+
+HashSet<MyClass> set = new HashSet<MyClass>() {obj1, obj2, obj3};
+System.Console.WriteLine(set.Count);
+
+public class MyClass
+{
+    public int ValueA { get; set; }
+    public int ValueB { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        MyClass other = (MyClass)obj;
+        return this.ValueA == other.ValueA && this.ValueB == other.ValueB;
+    }
+
+    public override int GetHashCode()
+    {
+        return ValueA.GetHashCode() ^ ValueB.GetHashCode();
+    }
+}
